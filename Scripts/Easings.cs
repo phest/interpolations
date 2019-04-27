cc5d467d2d4ce63677ea850b4c08ef01d4e0d67a
Steph Thirion <phest.contrib@gmail.com>
2019-04-27 16:23:14 -0700

    Scaffold library

---
+++ Scripts/Easings.cs
@@ -0,0 +1,13 @@
+// Easing equations Copyright (c) 2001 Robert Penner http://robertpenner.com/easing/
+// Open source under the MIT License. See LICENSE file for details.
+
+namespace Interpolations
+{
+    public static class I {
+
+        public static float Linear(float k) {
+            return k;
+        }
+        
+    }
+}
