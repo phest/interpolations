ec99c20d3f181b425d001f7218196a6ca531d492
Steph Thirion <phest.contrib@gmail.com>
2019-05-03 20:14:38 -0700

    Create ScaleTween class

---
+++ Scripts/Tweens/TransformTweens/ScaleTween.cs
@@ -0,0 +1,27 @@
+﻿// Interpolations - https://github.com/phest/interpolations
+// Copyright Steph Thirion - Licensed under the MIT license
+
+namespace Interpolations.Tweens
+{
+    using UnityEngine;
+    
+    public class ScaleTween : Tween<Vector3>
+    {
+        readonly Transform subject;
+
+        public ScaleTween(Transform subject)
+        {
+            this.subject = subject;
+        }
+
+        protected override void GetInitialValueFromSubject()
+        {
+            OriginValue = subject.localScale;
+        }
+
+        protected override void ApplyCurrentValueToSubject()
+        {
+            subject.localScale = Vector3.LerpUnclamped(OriginValue, TargetValue, ValueRatio);
+        }
+    }
+}
