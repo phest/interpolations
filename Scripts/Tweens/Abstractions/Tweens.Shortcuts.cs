469e79fbfa5953746cb0c4b87ddbe7fd17742fdc
Steph Thirion <phest.contrib@gmail.com>
2019-05-05 11:35:15 -0700

    Add optional unique bindings to shortcuts

--- Scripts/Tweens/Abstractions/Tweens.Shortcuts.cs
+++ Scripts/Tweens/Abstractions/Tweens.Shortcuts.cs
@@ -8,39 +8,41 @@ namespace Interpolations
 
     public static partial class Tweens
     {
-        public static Tween<float> RunFloat(Func<float> getter, Action<float> setter)
+        public static Tween<float> RunFloat(Func<float> getter, Action<float> setter, object uniqueBinding = null)
         {
-            return Run(new FloatTween(getter, setter));
+            return Run(new FloatTween(getter, setter), uniqueBinding);
         }
 
-        public static Tween<Vector3> RunVector3(Func<Vector3> getter, Action<Vector3> setter)
+        public static Tween<Vector3> RunVector3(Func<Vector3> getter, Action<Vector3> setter,
+            object uniqueBinding = null)
         {
-            return Run(new Vector3Tween(getter, setter));
+            return Run(new Vector3Tween(getter, setter), uniqueBinding);
         }
 
-        public static Tween<Quaternion> RunQuaternion(Func<Quaternion> getter, Action<Quaternion> setter)
+        public static Tween<Quaternion> RunQuaternion(Func<Quaternion> getter, Action<Quaternion> setter,
+            object uniqueBinding = null)
         {
-            return Run(new QuaternionTween(getter, setter));
+            return Run(new QuaternionTween(getter, setter), uniqueBinding);
         }
-        
-        public static Tween<Color> RunColor(Func<Color> getter, Action<Color> setter)
+
+        public static Tween<Color> RunColor(Func<Color> getter, Action<Color> setter, object uniqueBinding = null)
         {
-            return Run(new ColorTween(getter, setter));
+            return Run(new ColorTween(getter, setter), uniqueBinding);
         }
-        
-        public static Tween<Vector3> RunPosition(Transform transform, bool local = true)
+
+        public static Tween<Vector3> RunPosition(Transform transform, bool local = true, object uniqueBinding = null)
         {
-            return Run(new PositionTween(transform, local));
+            return Run(new PositionTween(transform, local), uniqueBinding);
         }
-        
-        public static Tween<Quaternion> RunRotation(Transform transform, bool local = true)
+
+        public static Tween<Quaternion> RunRotation(Transform transform, bool local = true, object uniqueBinding = null)
         {
-            return Run(new RotationTween(transform, local));
+            return Run(new RotationTween(transform, local), uniqueBinding);
         }
-        
-        public static Tween<Vector3> RunScale(Transform transform)
+
+        public static Tween<Vector3> RunScale(Transform transform, object uniqueBinding = null)
         {
-            return Run(new ScaleTween(transform));
+            return Run(new ScaleTween(transform), uniqueBinding);
         }
     }
 }
