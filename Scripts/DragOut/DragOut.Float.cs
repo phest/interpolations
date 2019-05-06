ef99a97c4329b9e5a8f74282463fb15bf0288375
Steph Thirion <phest.contrib@gmail.com>
2019-05-05 17:31:24 -0700

    Add first DragOut methods (float & Vector3)

---
+++ Scripts/DragOut/DragOut.Float.cs
@@ -0,0 +1,58 @@
+﻿// Interpolations - https://github.com/phest/interpolations
+// Copyright Steph Thirion - Licensed under the MIT license
+
+namespace Interpolations
+{
+    using UnityEngine;
+
+    public static partial class DragOutMethods
+    {
+        /// <summary>
+        /// Drags out a value. Useful to dampen magnitude of changes over time.
+        /// <param name="divisor">
+        /// Highest divisor, more drag.
+        /// Recommended above 1.
+        /// Can't be zero. 1 is no drag, and values less than 1 are unpredictable.
+        /// </param>
+        /// </summary>
+        public static float DragOut(this float current, float target, float divisor)
+        {
+            return current + (target - current) / divisor;
+        }
+
+        /// <summary>
+        /// Like <see cref="DragOut(float, float, float)"/>,
+        /// but with <paramref name="snap"/> and <paramref name="snapped"/>.
+        /// <param name="snap">
+        /// Will stop dragging out if difference between current and target is smaller than this value.
+        /// 0 or less doesn't snap.
+        /// </param>
+        /// <param name="snapped">
+        /// Will be set to true when snapping is applied.
+        /// </param>
+        /// </summary>
+        public static float DragOut(this float current, float target, float divisor, float snap, out bool snapped)
+        {
+            current = current.DragOut(target, divisor);
+            if (snap > 0 && Mathf.Abs(current - target) < snap)
+            {
+                current = target;
+                snapped = true;
+            }
+            else
+            {
+                snapped = false;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// Like <see cref="DragOut(float, float, float, float, out bool)"/>, without snapped output.
+        /// </summary>
+        public static float DragOut(this float current, float target, float divisor, float snap)
+        {
+            return DragOut(current, target, divisor, snap, out bool snapped);
+        }
+    }
+}
---
+++ Scripts/DragOut/DragOut.Vector3.cs
@@ -0,0 +1,46 @@
+﻿// Interpolations - https://github.com/phest/interpolations
+// Copyright Steph Thirion - Licensed under the MIT license
+
+namespace Interpolations
+{
+    using UnityEngine;
+
+    public static partial class DragOutMethods
+    {
+        /// <summary> 
+        /// <see cref="DragOut(float, float, float)"/>
+        /// </summary>
+        public static Vector3 DragOut(this Vector3 current, Vector3 target, float divisor)
+        {
+            return current + (target - current) / divisor;
+        }
+
+        /// <summary>
+        /// <see cref="DragOut(float, float, float, float, out bool)"/>
+        /// </summary>
+        public static Vector3 DragOut(this Vector3 current, Vector3 target, float divisor, float snap, out bool snapped)
+        {
+            current = current.DragOut(target, divisor);
+            if (snap > 0 && (current-target).magnitude < snap)
+            {
+                current = target;
+                snapped = true;
+            }
+            else
+            {
+                snapped = false;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// <see cref="DragOut(float, float, float, float)"/>
+        /// Version without final out parameter.
+        /// </summary>
+        public static Vector3 DragOut(this Vector3 current, Vector3 target, float divisor, float snap)
+        {
+            return DragOut(current, target, divisor, snap, out bool snapped);
+        }
+    }
+}
