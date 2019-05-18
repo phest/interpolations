44f5c21ff4e36aa0a47c55dc1df279d4d107d492
Steph Thirion <phest.contrib@gmail.com>
2019-05-17 22:44:01 -0700

    Add Runners API

---
+++ Scripts/Tweens/Runners/Runner.Shortcuts.cs
@@ -0,0 +1,54 @@
+﻿// Interpolations - https://github.com/phest/interpolations
+// Copyright Steph Thirion - Licensed under the MIT license
+
+namespace Interpolations
+{
+    using System;
+    using UnityEngine;
+
+    public partial class RunnerBase
+    {
+        public Tween<float> RunFloat(Func<float> getter, Action<float> setter, object uniqueBinding = null)
+        {
+            return Run(new FloatTween(getter, setter), uniqueBinding);
+        }
+
+        public Tween<Vector2> RunVector2(Func<Vector2> getter, Action<Vector2> setter,
+            object uniqueBinding = null)
+        {
+            return Run(new Vector2Tween(getter, setter), uniqueBinding);
+        }
+        
+        public Tween<Vector3> RunVector3(Func<Vector3> getter, Action<Vector3> setter,
+            object uniqueBinding = null)
+        {
+            return Run(new Vector3Tween(getter, setter), uniqueBinding);
+        }
+
+        public Tween<Quaternion> RunQuaternion(Func<Quaternion> getter, Action<Quaternion> setter,
+            object uniqueBinding = null)
+        {
+            return Run(new QuaternionTween(getter, setter), uniqueBinding);
+        }
+
+        public Tween<Color> RunColor(Func<Color> getter, Action<Color> setter, object uniqueBinding = null)
+        {
+            return Run(new ColorTween(getter, setter), uniqueBinding);
+        }
+
+        public Tween<Vector3> RunPosition(Transform otherTransform, bool local = true, object uniqueBinding = null)
+        {
+            return Run(new PositionTween(otherTransform, local), uniqueBinding);
+        }
+
+        public Tween<Quaternion> RunRotation(Transform otherTransform, bool local = true, object uniqueBinding = null)
+        {
+            return Run(new RotationTween(otherTransform, local), uniqueBinding);
+        }
+
+        public Tween<Vector3> RunScale(Transform otherTransform, object uniqueBinding = null)
+        {
+            return Run(new ScaleTween(otherTransform), uniqueBinding);
+        }
+    }
+}
---
+++ Scripts/Tweens/Runners/RunnerInstances.cs
@@ -0,0 +1,85 @@
+﻿// Interpolations - https://github.com/phest/interpolations
+// Copyright Steph Thirion - Licensed under the MIT license
+
+namespace Interpolations
+{
+    using UnityEngine;
+
+    public static class Tweens
+    {
+        static GameObject _runnerHolder;
+
+        static GameObject RunnersHolder
+        {
+            get
+            {
+                if (_runnerHolder == null)
+                {
+                    _runnerHolder = new GameObject("[ Tweens ]");
+                }
+
+                return _runnerHolder;
+            }
+        }
+
+        static Runner _runnerInstance;
+
+        public static Runner Runner
+        {
+            get
+            {
+                if (_runnerInstance == null)
+                {
+                    _runnerInstance = RunnersHolder.AddComponent<Runner>();
+                }
+
+                return _runnerInstance;
+            }
+        }
+
+        internal static void OnRunnerInstanceDestroy()
+        {
+            _runnerInstance = null;
+        }
+        
+        static FixedRunner _fixedRunnerInstance;
+
+        public static FixedRunner FixedRunner
+        {
+            get
+            {
+                if (_fixedRunnerInstance == null)
+                {
+                    _fixedRunnerInstance = RunnersHolder.AddComponent<FixedRunner>();
+                }
+
+                return _fixedRunnerInstance;
+            }
+        }
+
+        internal static void OnFixedRunnerInstanceDestroy()
+        {
+            _fixedRunnerInstance = null;
+        }
+        
+        static LateRunner _lateRunnerInstance;
+
+        public static LateRunner LateRunner
+        {
+            get
+            {
+                if (_lateRunnerInstance == null)
+                {
+                    _lateRunnerInstance = RunnersHolder.AddComponent<LateRunner>();
+                }
+
+                return _lateRunnerInstance;
+            }
+        }
+
+        internal static void OnLateRunnerInstanceDestroy()
+        {
+            _lateRunnerInstance = null;
+        }
+    }
+}
